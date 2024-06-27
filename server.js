958134b6b816490207e0a90cfff294dd3100975d
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-27 17:07:33 +0530

    Query strings (req.query)

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ let posts = [
 
 // Get all posts
 app.get("/api/posts", (req, res) => {
-  res.json(posts);
+  const limit = parseInt(req.query.limit);
+  if (!isNaN(limit) && limit > 0) {
+    res.json(posts.slice(0, limit));
+  } else {
+    res.json(posts);
+  }
 });
 // Get single post
 app.get("/api/posts/:id", (req, res) => {
