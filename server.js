7869a7c565aefd5e74940de78cd8aae7d664f8e8
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-27 16:14:32 +0530

    Static Web Server

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+
 
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
