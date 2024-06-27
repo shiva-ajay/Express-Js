fd767c6606e9c2a299041941613ef2fd0c2ae622
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-27 16:00:04 +0530

    res.sendFile() Method

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(8000, () => {
