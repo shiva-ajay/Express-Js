0fc93ff3acb5b403157cb6dcf239387f1fcf8258
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-28 11:37:22 +0530

    Route Flies

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const posts = require('./routes/posts')
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -7,32 +8,10 @@ const app = express();
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-  { id: 1, title: "Post One" },
-  { id: 2, title: "Post Two" },
-  { id: 3, title: "Post Three" },
-];
 
-// Get all posts
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  } 
-    res.status(200).json(posts);
-  
-});
-// Get single post
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find(post => post.id === id);
+// Routes
+app.use('/api/posts', posts)
 
-  if (!post) {
-   return res.status(404).json({ msg: `A post with the id of ${id} was not found` });
-  } 
-    res.status(200).json(post);
-  
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
