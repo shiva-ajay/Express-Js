8deae2567609795b9460b5733d1e2fbd401ee105
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-28 13:17:37 +0530

    DELETE Request

--- routes/posts.js
+++ routes/posts.js
@@ -59,5 +59,22 @@ router.put('/:id', (req, res) => {
 });
 
 
+//Update Post
+
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if (!post){
+        return res
+            .status(404)
+            .json({ msg: `A post with the id of ${id} was not found`});
+    }
+
+    posts = posts.filter((post) => post.id !== id);
+    res.status(200).json(posts);
+});
+
+
 export default router;
   
