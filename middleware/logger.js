915225df478061c9eb718f162b32995e01dd5925
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-28 14:55:47 +0530

    Color Package

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,21 @@
+import colors from 'colors';
+
 const logger = (req, res, next) => {
+  const methodColors = {
+    GET: 'green',
+    POST: 'blue',
+    PUT: 'yellow',
+    DELETE: 'red'
+  };
+
+  const method = req.method;
+  const color = methodColors[method] || 'white';
+
+
     console.log(
-      `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
+      `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}` [
+        color
+      ]
     );
     next();
   }
