93691a2547f7aad9e3b6c4e6ec86535768cdc2f3
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-28 16:48:00 +0530

    Final

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js'
 import logger from './middleware/logger.js'
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
 const port = process.env.PORT || 8000;
+
+
+//Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -18,7 +24,7 @@ app.use(logger);
 
 
 // setup static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Routes
