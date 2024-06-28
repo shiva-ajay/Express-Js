8b67f1c55956fd46027bf3e7a71fe6373af07579
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-28 15:49:17 +0530

    using Controllers

---
+++ controllers/postController.js
@@ -0,0 +1,77 @@
+// @dese   GET all posts
+// @route GET /api/posts
+
+export const getPosts = (req, res, next) => {
+  const limit = parseInt(req.query.limit);
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+  res.status(200).json(posts);
+};
+
+// @dese   GET single posts
+// @route GET /api/posts/:id
+
+export const getPost = (req, res, next) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
+  }
+  res.status(200).json(post);
+};
+
+// @dese   Create new post
+// @route   POST /api/posts
+
+export const createPOst = (req, res, next) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+  };
+
+  if (!newPost.title) {
+    const error = new Error(`Please include a title`);
+    error.status = 400;
+    return next(error);
+  }
+  posts.push(newPost);
+  res.status(201).json(posts);
+};
+
+// @dese   Update post
+// @route PUT /api/posts/:id
+
+export const updatePost = (req, res, next) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
+  }
+
+  post.title = req.body.title;
+  res.status(200).json(posts);
+};
+
+// @dese   Delete post
+// @route  DELETE /api/posts/:id
+
+export const deletePost = (req, res, next) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    const error = new Error(`A post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
+  }
+
+  posts = posts.filter((post) => post.id !== id);
+  res.status(200).json(posts);
+};
